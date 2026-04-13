fec13b078757e440292a62645fdf49a668b6ee43
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-13 15:11:21 +1200

    [feat] Implement delete button

--- routed-anecdotes/src/components/AnecdoteList.jsx
+++ routed-anecdotes/src/components/AnecdoteList.jsx
@@ -1,10 +1,18 @@
-const AnecdoteList = ({ anecdotes }) => (
+import { useAnecdotes } from "../hooks/useAnecdotes"
+
+const AnecdoteList = () => {
+  const {anecdotes, removeAnecdote} = useAnecdotes();
+  
+  return (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}>
+        {anecdote.content}
+        <button onClick={() => removeAnecdote(anecdote.id)}>Delete</button>
+      </li>)}
     </ul>
   </div>
-)
+)}
 
 export default AnecdoteList
